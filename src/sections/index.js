c3bf4e48e2d41cebdd0ee634914707d461a0f89e
Viraj <virajshewale21@gamil.com>
2023-10-05 15:16:28 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import CustomerReview from './CustomerReview';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReview,
+    Footer,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+    
+}
